b59aa8adeba392aadfd9a424b4b23c7a65082cc5
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-11-22 15:31:31 +0100

    Fixes incorrect install_paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,10 @@
-# Composer required install path to prevent installing directly under /
-default['composer']['install_path'] = "/opt/composer-libraries/phpcs";
-default['composer']['bin_dir']      = "/usr/local/bin"
-
 # PHP CodeSniffer
 default['phpcs']['package'] = 'squizlabs/php_codesniffer'
 default['phpcs']['version'] = '>=1.4'
+default['phpcs']['install_path'] = '/opt/composer-libraries/php_codesniffer'
+default['phpcs']['bin_dir'] = "/usr/local/bin"
 
 # CakePHP coding standard
 default['phpcs']['cakephp_package'] = 'cakephp/cakephp-codesniffer'
 default['phpcs']['cakephp_version'] = '0.1.*'
+default['phpcs']['cakephp_install_path'] = '/opt/composer-libraries/cakephp-codesniffer'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,18 +1,18 @@
 
 # Composer install PHP CodeSniffer
 composer_package 'PHP CodeSniffer' do
-  install_path node['composer']['install_path']
+  install_path node['phpcs']['install_path']
   packages ({ node['phpcs']['package'] => node['phpcs']['version'] })
   config ({
-    "bin-dir" => node['composer']['bin_dir']
+    "bin-dir" => node['phpcs']['bin_dir']
   })
   group "root"
   action [:create, :update]
 end
 
 # Composer install CakePHP coding standard
-composer_package 'PHP CodeSniffer' do
-  install_path node['composer']['install_path']
+composer_package 'CakePHP Coding Standard' do
+  install_path node['phpcs']['cakephp_install_path']
   packages ({ node['phpcs']['cakephp_package'] => node['phpcs']['cakephp_version'] })
   dev true
   group "root"
@@ -21,6 +21,6 @@ end
 
 # Enable CakePHP coding standard
 execute "Enable CakePHP CodeSniffer" do
-  command "phpcs --config-set installed_paths #{node['composer']['install_path']}/vendor/cakephp/cakephp-codesniffer"
+  command "phpcs --config-set installed_paths #{node['phpcs']['cakephp_install_path']}/vendor/cakephp/cakephp-codesniffer"
   action :run
 end
