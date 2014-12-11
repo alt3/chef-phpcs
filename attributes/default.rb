e47a2e89667c14502af48b6c950180d046930789
bravo-kernel <bravo-kernel@users.noreply.github.com>
2014-12-11 22:55:37 +0100

    Controlled bump to 2.*

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,10 @@
 # PHP CodeSniffer
 default['phpcs']['package'] = 'squizlabs/php_codesniffer'
-default['phpcs']['version'] = '>=1.4'
+default['phpcs']['version'] = '2.*'
 default['phpcs']['install_path'] = '/opt/composer-libraries/php_codesniffer'
 default['phpcs']['bin_dir'] = "/usr/local/bin"
 
 # CakePHP coding standard
 default['phpcs']['cakephp_package'] = 'cakephp/cakephp-codesniffer'
-default['phpcs']['cakephp_version'] = '0.1.*'
-default['phpcs']['cakephp_install_path'] = '/opt/composer-libraries/cakephp-codesniffer'
+default['phpcs']['cakephp_version'] = '2.0.*@dev'
+default['phpcs']['cakephp_install_path'] = '/opt/composer-libraries/cakephp_codesniffer'
